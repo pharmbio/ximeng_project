df3a6514715d6d9f3727f069e5b03689cf93eb46
XimYang <XimYang@users.noreply.github.com>
2020-11-04 23:33:35 +0000

    e

---
+++ prac.py
@@ -0,0 +1,9 @@
+#1. Write scripts to:
+#   a. Find the 3 biggest families in the data
+#   b. Find the 3 biggest group in the data
+#   c. Find the 3 biggest families that do not have overlap between each other in the data
+#   d. Find the 3 biggest group that do not have overlap between each other in the data
+#2. Next I want you to write a CNN that can discern between a control and non-control group using the biggest non-overlapping families/groups you found above
+#   a. I want you to write this using python, tensorflow2.0
+#   b. Check in your code regularly
+#   c. Contact me if you need more help to get going
