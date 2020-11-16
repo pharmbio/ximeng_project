1401a722628efcb28751c7723e866c2d17bb4718
XimYang <XimYang@users.noreply.github.com>
2020-11-16 15:26:40 +0000

    ignore

--- prac2.py
+++ prac2.py
@@ -33,25 +33,32 @@ big_group = list(unique_group_count.index[0:3])
 
 family_requried = dataframe['family'].map(lambda x : x in big_family)
 group_requried = dataframe['group'].map(lambda x : x in big_group)
-
+#subset dataframe of biggest families/groups
 chosendf = dataframe[family_requried & group_requried]
-file_name = str(list(chosendf['filename']))  #maybe array with label here not str?
-print(chosendf)
-
-
+#extract only needed filename and group, can be deleted later when rest works
+file_name = chosendf[['filename','group']]
+#extract image file names
 path_img='/home/jovyan/scratch-shared/ximeng/KinaseInhibitorData/MiSyHo299'
-ls = os.listdir(path_img)
+img_nms = os.listdir(path_img)
+img_nms = [i.replace('.png','') for i in img_nms]
 
-#can not locate line of chosendf
-for i in ls:
-    if bool(re.findall("\\b" + i +"\\b", file_name)):
-        if (chosendf.loc[chosendf.filename == "\\b" + i +"\\b"],'group') == "control": 
-            shutil.copy(path_img+'/'+i , "/home/jovyan/scratch-shared/ximeng/tfpre/control/"+i)
+#sort images into different classes
+for index,row in file_name.iterrows():
+    if row[0] in img_nms:
+        if row[1] == "control": 
+            shutil.copy(path_img+'/'+ row[0] + '.png' , "/home/jovyan/scratch-shared/ximeng/tfpre/control/"+row[0] + '.png')
         else:
-            shutil.copy(path_img+'/'+i , "/home/jovyan/scratch-shared/ximeng/tfpre/non_control/"+i)
+            shutil.copy(path_img+'/'+ row[0] + '.png' , "/home/jovyan/scratch-shared/ximeng/tfpre/non_control/"+row[0] + '.png')
+
+#Then every thing goes into non_control group(this one has a weird error now, but similar partin prac.py would work), this made me frustrate.
+#BUT, I just took a detailed look at the extracted datarame(file_name), it looks like all chosen data are TK group??
+#Now I feel I WASTED WHOLE week...
+
+#Or have I made some mistake?
 
 
 
+#the rest part can be ignore so far
 cwd='/home/jovyan/scratch-shared/ximeng/tfpre/'
 classes={'control','non_control'}
 writer= tf.python_io.TFRecordWriter("msh_test.tfrecords")
