ae94ae7ebde03321823e063a7c701a2b5047b388
XimYang <XimYang@users.noreply.github.com>
2021-01-17 20:45:27 +0000

    weird error

--- load_csv.py
+++ load_csv.py
@@ -1,6 +1,7 @@
 import torch
 import pandas as pd
 import PIL
+from PIL import Image
 import os
 import csv
 
@@ -13,9 +14,9 @@ class CustomDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.df)
     def __getitem__(self, index):
-        filename = self.df[index,'id']
-        label = self.df[index, 'type']
-        image = PIL.Image.open(os.path.join(self.images_folder, filename,'.tif'))
+        filename = self.df.loc[index,'id']
+        label = self.df.loc[index, 'type']
+        image = PIL.Image.open(os.path.join(self.images_folder, str(filename)) + '.tif')
         if self.transform is not None:
             image = self.transform(image)
         return image, label
@@ -24,4 +25,5 @@ train_dataset = CustomDataset("/home/jovyan/mnt/external-images-pvc/ximeng/csv_f
 test_dataset = CustomDataset("/home/jovyan/mnt/external-images-pvc/ximeng/csv_files_for_load/test1_test_dataset.csv", "/home/jovyan/mnt/external-images-pvc/ximeng/all_tif_image_files_five_channels"  )
 valid_dataset = CustomDataset("/home/jovyan/mnt/external-images-pvc/ximeng/csv_files_for_load/test1_valid_dataset.csv", "/home/jovyan/mnt/external-images-pvc/ximeng/all_tif_image_files_five_channels"  )
 
-image, label = valid_dataset[3]
+print(image, label = valid_dataset[3])
+print(os.path.join("/home/jovyan/mnt/external-images-pvc/ximeng/all_tif_image_files_five_channels", str(3)) + '.tif')
--- new_test_pytorch.py
+++ new_test_pytorch.py
@@ -12,19 +12,20 @@ from torchvision.datasets import ImageFolder
 
 class CustomDataset(torch.utils.data.Dataset):
     def __init__(self, csv_path, images_folder, transform = None):
-        self.df = pd.read_csv(csv_path)
+        self.df = pd.read_csv(csv_path,sep = ';')
         self.images_folder = images_folder
         self.transform = transform
 
     def __len__(self):
         return len(self.df)
     def __getitem__(self, index):
-        filename = self.df[index, "id"]
-        label = self.df[index, "type"]
-        image = PIL.Image.open(os.path.join(self.images_folder, filename,'.tif'))
+        filename = self.df.loc[index,'id']
+        label = self.df.loc[index, 'type']
+        image = PIL.Image.open(os.path.join(self.images_folder, str(filename)) + '.tif')
         if self.transform is not None:
             image = self.transform(image)
         return image, label
+       
        
 train_dataset = CustomDataset("/home/jovyan/mnt/external-images-pvc/ximeng/csv_files_for_load/test1_train_dataset.csv", "/home/jovyan/mnt/external-images-pvc/ximeng/all_tif_image_files_five_channels"  )
 valid_dataset = CustomDataset("/home/jovyan/mnt/external-images-pvc/ximeng/csv_files_for_load/test1_valid_dataset.csv", "/home/jovyan/mnt/external-images-pvc/ximeng/all_tif_image_files_five_channels"  )
@@ -71,16 +72,12 @@ def train_and_valid(model, loss_function, optimizer, epochs=10):
         for i, (inputs, labels) in enumerate(train_dataset):
             inputs = inputs.to(device)
             labels = labels.to(device)
- 
-            #Because the gradient here is cumulative, so remember to clear it every time
+
             optimizer.zero_grad()
- 
             outputs = model(inputs)
  
             loss = loss_function(outputs, labels)
- 
             loss.backward()
- 
             optimizer.step()
  
             train_loss += loss.item() * inputs.size(0)
